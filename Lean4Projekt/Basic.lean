4062d343c26510a899329ec5ff0984635f1d1ea2
zNoctum <noah.duemling@gmail.com>
2025-06-27 03:15:46 +0200

    Implement diagonal dominance as a convergence criteria for jacobi iterations

--- Lean4Projekt/Basic.lean
+++ Lean4Projekt/Basic.lean
@@ -2,6 +2,7 @@ import Mathlib.Analysis.Matrix
 import Mathlib.Analysis.Normed.Algebra.Spectrum
 import Mathlib.Analysis.Normed.Field.Basic
 import Mathlib.Analysis.NormedSpace.OperatorNorm.Basic
+import Mathlib.Analysis.NormedSpace.OperatorNorm.NNNorm
 import Mathlib.Data.Matrix.Notation
 import Mathlib.Data.Matrix.Reflection
 import Mathlib.Data.Finset.Basic
@@ -10,10 +11,10 @@ import Mathlib.Data.Fintype.Basic
 import Mathlib.Topology.Basic
 import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Topology.MetricSpace.Contracting
-import Mathlib.LinearAlgebra.AffineSpace.AffineMap
 import Mathlib.LinearAlgebra.Matrix.Block
 import Mathlib.LinearAlgebra.Matrix.ToLin
 import Mathlib.LinearAlgebra.FiniteDimensional.Basic
+import Mathlib.Algebra.Group.Invertible.Basic
 
 open Matrix
 
@@ -45,7 +46,7 @@ theorem iter_conv (it : ConvIter ι 𝕜) (v : ι → 𝕜):
   let ⟨x, _, hr, _⟩ := ContractingWith.exists_fixedPoint (iter_contracting it) v (edist_ne_top v (it.toFun v))
   exact ⟨x, hr⟩
 
-theorem helper (it : ConvIter ι 𝕜) (x : ι → 𝕜) (heq: it.A *ᵥ x = it.b):
+theorem solution_is_fixpt (it : ConvIter ι 𝕜) (x : ι → 𝕜) (heq: it.A *ᵥ x = it.b):
     Function.IsFixedPt it.toFun x := by
   let inv := it.inv
   rw [it.eq, add_mulVec] at heq
@@ -57,48 +58,84 @@ theorem helper (it : ConvIter ι 𝕜) (x : ι → 𝕜) (heq: it.A *ᵥ x = it.
   simp only [ConvIter.toFun, neg_mul]
   rw [add_comm, ←heq]
 
-theorem iter_tendsto (it : ConvIter ι 𝕜) (v x : ι → 𝕜) (h: it.A *ᵥ x = it.b):
+theorem iter_tendsto_solution (it : ConvIter ι 𝕜) (v x : ι → 𝕜) (h: it.A *ᵥ x = it.b):
     Filter.Tendsto (fun n ↦ it.toFun^[n] v) Filter.atTop (nhds x) := by
   let ⟨x', hic⟩ := iter_conv it v
   have : Filter.Tendsto (fun (n:Nat) => x) Filter.atTop (nhds x) := tendsto_const_nhds
   have hfe: (fun n => it.toFun^[n] x) = (fun n => x) := by
     funext n
-    exact Function.iterate_fixed (helper it x h) n
+    exact Function.iterate_fixed (sol_is_fixpt it x h) n
   rw [←hfe] at this
   have hf : ContractingWith ‖(-it.M⁻¹ * it.N).toLin'.toContinuousLinearMap‖₊ it.toFun :=
     iter_contracting it
   let hxfp := ContractingWith.tendsto_iterate_fixedPoint hf v
-  rw [←ContractingWith.fixedPoint_unique hf (helper it x h)] at hxfp
+  rw [←ContractingWith.fixedPoint_unique hf (sol_is_fixpt it x h)] at hxfp
   assumption
 
 def diag_dominant (M : Matrix ι ι 𝕜) :=
-  ∀ i : ι, (∑ j ∈ Finset.univ.erase i, ‖M i j‖) < ‖M i i‖
+  ∀ i : ι, (∑ j ∈ Finset.univ.erase i, ‖M i j‖₊) < ‖M i i‖₊
 
-variable (M : Matrix ι ι 𝕜) (b : ι → 𝕜)
+variable (M : Matrix ι ι 𝕜) (b : ι → 𝕜) [LE 𝕜] [OrderBot 𝕜] [NormedAlgebra ℝ 𝕜]
 
-noncomputable def jacobi (hd : diag_dominant M) : ConvIter ι 𝕜 := {
-  A := M
-  M := (diagonal M.diag)
-  b := b
-  inv := by
-    apply invertibleOfIsUnitDet
-    rw [det_diagonal]
-    apply isUnit_iff_ne_zero.mpr
-    rw [Finset.prod_ne_zero_iff]
+noncomputable instance : NormedAddCommGroup (Matrix ι ι 𝕜) := Matrix.linftyOpNormedAddCommGroup
+instance : NormedSpace 𝕜 (Matrix ι ι 𝕜) := Matrix.linftyOpNormedSpace
+
+noncomputable def jacobi (hd : diag_dominant M) : ConvIter ι 𝕜 := by
+  have hnz : ∀ i ∈ Finset.univ, M.diag i ≠ 0 := by
     intro i _
     by_contra! h
     simp at *
     dsimp [diag_dominant] at *
     specialize hd i
     simp [h] at hd
-    have h : 0 ≤ ∑ j, ‖M i j‖ := by
-      apply Finset.sum_nonneg
-      intro l
-      simp
-    exact lt_irrefl 0 (lt_of_le_of_lt h hd)
-  spec := by
-    sorry
-}
+  haveI : Invertible (diagonal M.diag) := by
+    apply invertibleOfIsUnitDet
+    rw [det_diagonal]
+    apply isUnit_iff_ne_zero.mpr
+    rw [Finset.prod_ne_zero_iff]
+    exact hnz
+  exact {
+    A := M
+    M := (diagonal M.diag)
+    b := b
+    inv := by infer_instance
+    spec := by
+      rw [← linfty_opNNNorm_toMatrix, LinearMap.coe_toContinuousLinearMap, LinearMap.toMatrix'_toLin']
+      rw [Matrix.linfty_opNNNorm_def, Finset.sup_lt_iff (by simp)]
+      intro i _
+      have h : M - diagonal M.diag = of fun i j => if i = j then 0 else M i j := by
+        funext i j
+        by_cases h : i = j <;> simp [sub_apply, h]
+      let f := fun j => ‖((diagonal M.diag)⁻¹ * (of fun i j => if i = j then 0 else M i j)) i j‖₊
+      calc ∑ j, ‖(-(diagonal M.diag)⁻¹ * (M - diagonal M.diag)) i j‖₊ = _ := by rfl
+        _ = ∑ j, ‖(-(diagonal M.diag)⁻¹ * (of fun i j => if i = j then 0 else M i j)) i j‖₊ := by rw [h]
+        _ = ∑ j, ‖((diagonal M.diag)⁻¹ * (of fun i j => if i = j then 0 else M i j)) i j‖₊ := by simp
+        _ = ∑ j, f j := by simp [f]
+        _ = ∑ j, f j - f i := by simp [f, inv_diagonal, diagonal_mul, of_apply]
+        _ = ∑ j ∈ Finset.univ.erase i, f j := by simp [←Finset.sum_erase_add (s := Finset.univ) (a := i)]
+        _ = ∑ j ∈ Finset.univ.erase i, ‖(M i i)⁻¹ * M i j‖₊ := by
+          dsimp [f]
+          apply Finset.sum_congr (by rfl)
+          intro j hj
+          rw [inv_diagonal, diagonal_mul, of_apply]
+          rw [if_neg (Finset.ne_of_mem_erase hj).symm]
+          have hu : IsUnit M.diag := by
+            refine isUnit_diagonal.mp ?_
+            exact isUnit_of_invertible (diagonal M.diag)
+          rw [Ring.inverse_of_isUnit hu]
+          simp
+        _ = ∑ j ∈ Finset.univ.erase i, ‖M i i‖₊⁻¹ * ‖M i j‖₊ := by simp
+        _ = (∑ j ∈ Finset.univ.erase i, ‖M i j‖₊) * ‖M i i‖₊⁻¹ := by
+          rw [Finset.sum_mul (Finset.univ.erase i)]
+          apply Finset.sum_congr (by rfl)
+          intro _ _
+          rw [mul_comm]
+      rw [←NNReal.lt_inv_iff_mul_lt]
+      · simpa using (hd i)
+      · apply inv_ne_zero
+        rw [←diag_apply M, nnnorm_ne_zero_iff]
+        exact hnz i (Finset.mem_univ i)
+  }
 
 variable [LinearOrder ι] [DecidableLT ι] [LocallyFiniteOrderBot ι] [LocallyFiniteOrderTop ι]
 
